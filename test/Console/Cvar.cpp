5b7536276981a20dcb2547057ec68a7383e08d91
throwaway47912 <throwaway47912@gmail.com>
2021-01-15 13:40:26 +0100

    Add Cvar tests for setting invalid value

--- test/Console/Cvar.cpp
+++ test/Console/Cvar.cpp
@@ -1,7 +1,12 @@
 #include "GameLibrary/Console/Cvar.h"
 
+#include <limits>
+
 #include "catch2/catch.hpp"
 
+#include "GameLibrary/Exceptions/Standard.h"
+
+using namespace GameLibrary;
 using namespace GameLibrary::Console;
 
 /*
@@ -47,6 +52,15 @@ TEST_CASE("Cvar sets and returns the correct value, throwing on failure.")
 			c.set(-0.0);
 			REQUIRE(c.getAsString() == "-0");
 		}
+
+		SECTION("Invalid arguments")
+		{
+			Cvar c(Cvar::ValueType::Float, 0);
+
+			REQUIRE_THROWS_AS(c.set(std::numeric_limits<float>::quiet_NaN()), Exceptions::ConversionError);
+			REQUIRE_THROWS_AS(c.set("12.34 invalid"), Exceptions::ConversionError);
+			REQUIRE_THROWS_AS(c.set(nullptr), Exceptions::InvalidArgument);
+		}
 	}
 
 	SECTION("Integer Cvar (Float->Integer should truncate)")
@@ -79,6 +93,15 @@ TEST_CASE("Cvar sets and returns the correct value, throwing on failure.")
 
 			c.set("-1.99999");
 			REQUIRE(c.getAsString() == "-1");
+		}
+
+		SECTION("Invalid Arguments")
+		{
+			Cvar c(Cvar::ValueType::Integer, 0);
+
+			REQUIRE_THROWS_AS(c.set(std::numeric_limits<float>::quiet_NaN()), Exceptions::ConversionError);
+			REQUIRE_THROWS_AS(c.set("1234 invalid"), Exceptions::ConversionError);
+			REQUIRE_THROWS_AS(c.set(nullptr), Exceptions::InvalidArgument);
 		}
 	}
 
