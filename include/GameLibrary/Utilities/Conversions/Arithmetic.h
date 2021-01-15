cffd3963f590727fe420bb4b80dc575f9341d36e
throwaway47912 <throwaway47912@gmail.com>
2021-01-15 12:37:16 +0100

    Make safeArithmeticCast() throw on invalid floats

--- include/GameLibrary/Utilities/Conversions/Arithmetic.h
+++ include/GameLibrary/Utilities/Conversions/Arithmetic.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #include <boost/numeric/conversion/cast.hpp>
 
 #include "GameLibrary/Exceptions/Conversions.h"
@@ -9,6 +11,13 @@ namespace GameLibrary::Utilities::Conversions
 {
 	template<typename To, typename From>
 	To safeArithmeticCast(From from) {
+		if constexpr (std::is_floating_point_v<From>)
+		{
+			const auto category = std::fpclassify(from);
+			if (category != FP_NORMAL && category != FP_ZERO && category != FP_SUBNORMAL)
+				throw Exceptions::ConversionError::fromTypes<From, To>("safeArithmeticCast() failed: invalid floating-point argument.");
+		}
+
 		try
 		{
 			return boost::numeric_cast<To>(from);
