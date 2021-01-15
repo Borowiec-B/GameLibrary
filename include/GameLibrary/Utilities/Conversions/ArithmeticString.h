1dfabc7f65326097d6234c9874a40d9b172aaa0b
throwaway47912 <throwaway47912@gmail.com>
2021-01-15 17:18:51 +0100

    Add arithmeticOrStringCast()

---
+++ include/GameLibrary/Utilities/Conversions/ArithmeticString.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "GameLibrary/Exceptions/Conversions.h"
+#include "GameLibrary/Utilities/Conversions/Arithmetic.h"
+#include "GameLibrary/Utilities/Conversions/String.h"
+
+
+namespace GameLibrary::Utilities::Conversions
+{
+	/*
+	 *  arithmeticOrStringCast(): Perform conversion Arithmetic/String -> Arithmetic/String.
+	 *
+	 *  Guidelines (each position is an if-else):
+	 *    - To == From (both decayed):	return from.
+	 *    - Both types are arithmetic:	perform safeArithmeticCast()
+	 *    - To is String:				perform toString() on from.
+	 *    - From is String:				perform fromString() on from.
+	 *
+	 *  Throws:
+	 *    - ConversionError if any underlying conversion functions throw.
+	 */
+	template<typename To, typename From>
+	std::enable_if_t<(std::is_arithmetic_v<To> || IsStringV<To>) && (std::is_arithmetic_v<From> || IsStringV<From>), 
+	To>
+	arithmeticOrStringCast(From&& from) {
+		try
+		{
+			if constexpr (std::is_same_v<std::decay_t<To>, std::decay_t<From>>)
+				return std::forward<From>(from);
+			else if constexpr (std::is_arithmetic_v<To> && std::is_arithmetic_v<From>)
+				return safeArithmeticCast<To>(std::forward<From>(from));
+			else if constexpr (IsStringV<To>)
+				return toString<To>(std::forward<From>(from));
+			else if constexpr (IsStringV<From>)
+				return fromString<To>(std::forward<From>(from));
+		}
+		catch (const std::exception&)
+		{
+			throw Exceptions::ConversionError::fromTypes<From, To>();
+		}
+	}
+}
---
+++ test/Utilities/Conversions/ArithmeticString.cpp
@@ -0,0 +1,24 @@
+#include "GameLibrary/Utilities/Conversions/ArithmeticString.h"
+
+#include <limits>
+#include <string>
+
+#include "catch2/catch.hpp"
+
+using namespace GameLibrary;
+using namespace GameLibrary::Utilities::Conversions;
+
+
+TEST_CASE("arithmeticOrStringCast() sticks to guidelines (calls correct casting functions).")
+{
+	REQUIRE(arithmeticOrStringCast<int>(1.9999) == safeArithmeticCast<int>(1.9999));
+	REQUIRE(arithmeticOrStringCast<std::wstring>(std::numeric_limits<float>::infinity()) == toString<std::wstring>(std::numeric_limits<float>::infinity()));
+
+	const auto toWStringLDMax = toString<std::wstring>(std::numeric_limits<long double>::max(), FloatPrecisionPreset::Max);
+	const auto fromWStringLDMax = fromString<long double>(toWStringLDMax);
+	REQUIRE(arithmeticOrStringCast<long double>(toWStringLDMax) == fromWStringLDMax);
+
+	REQUIRE_THROWS_AS(arithmeticOrStringCast<uint8_t>(std::numeric_limits<uint16_t>::max()), Exceptions::ConversionError);
+	REQUIRE_THROWS_AS(arithmeticOrStringCast<int>(std::numeric_limits<float>::quiet_NaN()), Exceptions::ConversionError);
+	REQUIRE_THROWS_AS(arithmeticOrStringCast<int>(std::wstring(L"1 invalid")), Exceptions::ConversionError);
+}
