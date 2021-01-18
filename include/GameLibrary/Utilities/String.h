130e62937258afa7f6fb25bd888bd9840da5e3d7
throwaway47912 <throwaway47912@gmail.com>
2021-01-18 15:57:08 +0100

    Remove duplicate code (already in Traits.h)

--- include/GameLibrary/Utilities/String.h
+++ include/GameLibrary/Utilities/String.h
@@ -5,18 +5,6 @@
 
 namespace GameLibrary::Utilities
 {
-	template<typename T>
-	struct IsString : public std::disjunction<
-					   std::is_same<std::decay_t<T>, char*>,
-					   std::is_same<std::decay_t<T>, const char*>,
-					   std::is_same<std::decay_t<T>, wchar_t*>,
-					   std::is_same<std::decay_t<T>, const wchar_t*>,
-					   std::is_same<std::decay_t<T>, std::string>,
-					   std::is_same<std::decay_t<T>, std::wstring>
-	> {};
-	template<typename T>
-	inline constexpr bool IsStringV = IsString<T>::value;
-
 	template<typename S1, typename S2>
 	S1 surround(const S1& str, const S2& surrounder) {
 		auto stream = Conversions::stringToOstringstream<S1>();
