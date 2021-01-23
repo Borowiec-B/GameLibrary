01464d96a5c4ea4f768a4432049fd4ac44762a14
throwaway47912 <throwaway47912@gmail.com>
2021-01-23 13:28:53 +0100

    Add Exceptions::NotFoundError

--- include/GameLibrary/Exceptions/Standard.h
+++ include/GameLibrary/Exceptions/Standard.h
@@ -8,4 +8,8 @@ namespace GameLibrary::Exceptions
 	class OverflowError : public std::overflow_error {
 		using std::overflow_error::overflow_error;
 	};
+
+	class NotFoundError : public std::runtime_error {
+		using std::runtime_error::runtime_error;
+	};
 }
