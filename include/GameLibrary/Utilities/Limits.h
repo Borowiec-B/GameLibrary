af7b0a21b147a1a7d6acdd0ba9cb0f0973b6f59c
throwaway47912 <throwaway47912@gmail.com>
2021-01-23 13:10:27 +0100

    Add Utilities::additionWillOverflow()

---
+++ include/GameLibrary/Utilities/Limits.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <limits>
+
+
+
+
+
+
+#include <iostream>
+
+
+
+namespace GameLibrary::Utilities
+{
+	template<typename T1, typename T2>
+	constexpr bool additionWillOverflow(const T1 x, const T2 y) noexcept {
+		if (y < 0)
+		{
+			if ( (std::numeric_limits<T1>::lowest() - y) > x )
+			{
+				return true;
+			}
+		}
+		else
+		{
+			if ( (std::numeric_limits<T1>::max() - y) < x )
+				return true;
+		}
+
+		return false;
+	}
+}
+
