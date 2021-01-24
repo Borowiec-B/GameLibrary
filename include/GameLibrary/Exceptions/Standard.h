4479d4c771732c9a24434a9d3a02b961f0f03a50
throwaway47912 <throwaway47912@gmail.com>
2021-01-24 12:27:37 +0100

    Format exception definitions alphabetically

--- include/GameLibrary/Exceptions/Standard.h
+++ include/GameLibrary/Exceptions/Standard.h
@@ -5,15 +5,15 @@
 
 namespace GameLibrary::Exceptions
 {
-	class OverflowError : public std::overflow_error {
-		using std::overflow_error::overflow_error;
+	class InvalidArgument : public std::invalid_argument {
+		using std::invalid_argument::invalid_argument;
 	};
 
 	class NotFoundError : public std::runtime_error {
 		using std::runtime_error::runtime_error;
 	};
 
-	class InvalidArgument : public std::invalid_argument {
-		using std::invalid_argument::invalid_argument;
+	class OverflowError : public std::overflow_error {
+		using std::overflow_error::overflow_error;
 	};
 }
